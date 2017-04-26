11f1056166ad0d93aa7a628b45e36ba7fe05bd3b
Diana <dchumpitazm@gmail.com>
2017-04-26 09:49:42 -0500

    productos: Agregar, Modificar, Eliminar, Cargar desde archivo

---
+++ Gimnasio/ConsoleApp1/CLASES/Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.CLASES
+{
+    public class Producto
+    {
+        private int _idProducto;
+        private string _nombreProducto;
+        private string _descripcion;
+        private int _stock;
+        private float _precio;
+
+        public Producto()
+        {
+
+        }
+
+        public Producto(int idProducto, string nombreProducto, string descripcion, int stock, float precio)
+        {
+            _idProducto = idProducto;
+            _nombreProducto = nombreProducto;
+            _descripcion = descripcion;
+            _stock = stock;
+            _precio = precio;
+        }
+
+        public int IdProducto { get => _idProducto; set => _idProducto = value; }
+        public string NombreProducto { get => _nombreProducto; set => _nombreProducto = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public int Stock { get => _stock; set => _stock = value; }
+        public float Precio { get => _precio; set => _precio = value; }
+    }
+}
--- Gimnasio/ConsoleApp1/CLASES/Program.cs
+++ Gimnasio/ConsoleApp1/CLASES/Program.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using ConsoleApp1.FORMULARIOS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 namespace ConsoleApp1
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmAgregarProducto());
         }
     }
 }
